d1e72d9489148f19fbd10bdf471094bf81ccec5b
deon-l <deon.liu@outlook.com>
2026-07-10 23:19:13 -0400

    fix: Add options for each spinner

--- app/src/main/java/com/cscb07/taamapp/EditArtifactFragment.java
+++ app/src/main/java/com/cscb07/taamapp/EditArtifactFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,7 +15,13 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EditArtifactFragment extends Fragment {
+    private boolean isEditing;
+    private boolean isModeAdd() { return !isEditing; }
 
     TextView textViewLotNumber;
     EditText
@@ -34,6 +41,19 @@ public class EditArtifactFragment extends Fragment {
             spinnerArtifactMaterial,
             spinnerDynasty;
 
+
+    public static final String SPINNER_DEFAULT_CATEGORY = "select category:";
+    public static final String SPINNER_DEFAULT_MATERIAL = "select material:";
+    public static final String SPINNER_DEFAULT_DYNASTY = "select dynasty/period:";
+    private void initSpinner(Spinner spin, String default_value, int arrayId)
+    {
+        List<CharSequence> options = new ArrayList<>(Arrays.asList(getResources().getStringArray(arrayId)));
+        options.add(0, default_value);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -44,8 +64,11 @@ public class EditArtifactFragment extends Fragment {
         editTextName = view.findViewById(R.id.editTextName);
         editTextArtifactDescription = view.findViewById(R.id.editTextArtifactDescription);
         spinnerArtifactCategory = view.findViewById(R.id.spinnerArtifactCategory);
+        initSpinner(spinnerArtifactCategory, SPINNER_DEFAULT_CATEGORY, R.array.categories_array);
         spinnerArtifactMaterial = view.findViewById(R.id.spinnerArtifactMaterial);
+        initSpinner(spinnerArtifactMaterial, SPINNER_DEFAULT_MATERIAL, R.array.material_array);
         spinnerDynasty = view.findViewById(R.id.spinnerDynasty);
+        initSpinner(spinnerDynasty, SPINNER_DEFAULT_DYNASTY, R.array.dynasty_period_array);
         editTextCulturalOrigin = view.findViewById(R.id.editTextCulturalOrigin);
         editTextDimensions = view.findViewById(R.id.editTextDimensions);
         editTextConditionReport = view.findViewById(R.id.editTextConditionReport);
@@ -61,6 +84,7 @@ public class EditArtifactFragment extends Fragment {
         buttonCancel.setOnClickListener(v -> getParentFragmentManager().popBackStack());
         // TODO: set up save button behaviour.
         buttonSave.setOnClickListener(v -> Toast.makeText(getContext(), "Saving! (not really)", Toast.LENGTH_SHORT).show());
+
 
         return view;
     }

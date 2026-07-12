2dd1f4fd5e15e5277859c8dab3dc235398fce5ac
deon-l <deon.liu@outlook.com>
2026-07-11 23:11:03 -0400

    test: Added `fragment-testing` dependancy and added instrumented tests for EditArtifactFragment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation(libs.fragment.testing)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)

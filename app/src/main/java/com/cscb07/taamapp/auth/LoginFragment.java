cba238df8eeaed932f6135b37f6262cd9b66b44a
Daniel <250892580+danielohlord@users.noreply.github.com>
2026-07-13 13:16:28 -0400

    Fixed XML issue after duplicating fragment_login

--- app/src/main/java/com/cscb07/taamapp/auth/LoginFragment.java
+++ app/src/main/java/com/cscb07/taamapp/auth/LoginFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +16,12 @@ import com.cscb07.taamapp.R;
 public class LoginFragment extends Fragment {
 
     private EditText editTextEmail, editTextPassword;
+    private Button loginButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        editTextEmail = view.findViewById(R.id.Login)
 
         return view;
     }

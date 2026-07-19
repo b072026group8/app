139dd87272901e30fb7a27466e49342ae559f8de
deon-l <deon.liu@outlook.com>
2026-07-18 21:39:33 -0400

    feature: Implemented `FireSupaDbEditorAccess` methods for adding items

--- app/src/main/java/com/cscb07/taamapp/FireSupaDbEditorAccess.java
+++ app/src/main/java/com/cscb07/taamapp/FireSupaDbEditorAccess.java
@@ -2,9 +2,15 @@ package com.cscb07.taamapp;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Default implementation of {@link DbEditorAccess} to use with {@link EditArtifactFragment}.
@@ -13,15 +19,36 @@ public class FireSupaDbEditorAccess implements DbEditorAccess{
     public static final String TAG = "FireSupaDbEditorAccess";
     private final FirebaseDatabase db;
     private final DatabaseReference dbRef;
+    @Nullable
+    private DatabaseReference addedItemRef;
+    @Nullable
+    private AddedItemChangeListener addChangeListener = null;
 
     public FireSupaDbEditorAccess() {
         db = FirebaseDatabase.getInstance();
         dbRef = db.getReference("artifacts/");
     }
+
+    private void removeAddedKey() {
+        if (addedItemRef != null && addChangeListener != null) {
+            if (addChangeListener.wasChanged()) {
+                Log.w(TAG, "It seems that a reserved item was modified before push. Not removing.");
+            } else {
+                addedItemRef.removeValue();
+            }
+            addedItemRef.removeEventListener(addChangeListener);
+        }
+
+        addedItemRef = null;
+        addChangeListener = null;
+    }
     @Override
     public String getUniqueLotNumber() {
-        Log.wtf(TAG, "not implemented");
-        return "Not Implemented";
+        removeAddedKey();
+        addedItemRef = dbRef.push();
+        Log.i(TAG, "Adding item - created key: " + addedItemRef.getKey());
+
+        return addedItemRef.getKey();
     }
 
     @Override
@@ -32,7 +59,34 @@ public class FireSupaDbEditorAccess implements DbEditorAccess{
     }
 
     @Override
-    public void cancelAdd(String lotNumber) {
-        Log.wtf(TAG, "not implemented");
+    public void cancelAdd(@NonNull String lotNumber) {
+        if (addedItemRef == null) {
+            return;
+        }
+        if (!lotNumber.isEmpty()) {
+            if (!lotNumber.equals(addedItemRef.getKey())) {
+                Log.wtf(TAG, "Mismatch between given lot and recorded lot of reserved added item. Not Removing.");
+            } else {
+                removeAddedKey();
+            }
+        }
+    }
+
+    private static class AddedItemChangeListener implements ValueEventListener {
+        private boolean wasChanged = false;
+        public boolean wasChanged() {
+            return wasChanged;
+        }
+
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            wasChanged = true;
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Log.w(TAG, "listener removed with error: " + error);
+        }
+
     }
 }

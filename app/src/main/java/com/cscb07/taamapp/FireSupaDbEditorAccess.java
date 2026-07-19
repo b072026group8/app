5afc0af006cc852c7d1d31b7b6fb2782b68396d0
deon-l <deon.liu@outlook.com>
2026-07-18 21:15:05 -0400

    feature: Implemented updating items  in `FireSupaDbEditorAccess`

--- app/src/main/java/com/cscb07/taamapp/FireSupaDbEditorAccess.java
+++ app/src/main/java/com/cscb07/taamapp/FireSupaDbEditorAccess.java
@@ -2,11 +2,22 @@ package com.cscb07.taamapp;
 
 import android.util.Log;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
- * Default implemented of {@link DbEditorAccess} to use with {@link EditArtifactFragment}.
+ * Default implementation of {@link DbEditorAccess} to use with {@link EditArtifactFragment}.
  */
 public class FireSupaDbEditorAccess implements DbEditorAccess{
     public static final String TAG = "FireSupaDbEditorAccess";
+    private final FirebaseDatabase db;
+    private final DatabaseReference dbRef;
+
+    public FireSupaDbEditorAccess() {
+        db = FirebaseDatabase.getInstance();
+        dbRef = db.getReference("artifacts/");
+    }
     @Override
     public String getUniqueLotNumber() {
         Log.wtf(TAG, "not implemented");
@@ -15,8 +26,9 @@ public class FireSupaDbEditorAccess implements DbEditorAccess{
 
     @Override
     public DbEditorAccessResult editItem(Item item) {
-        Log.wtf(TAG, "not implemented");
-        return DbEditorAccessResult.ERROR;
+        Task<Void> task = dbRef.child(item.getLotNumber()).setValue(item);
+        task.addOnSuccessListener(v -> Log.i(TAG, "Updated item. LOT: " + item.getLotNumber()));
+        return DbEditorAccessResult.SUCCESS;
     }
 
     @Override

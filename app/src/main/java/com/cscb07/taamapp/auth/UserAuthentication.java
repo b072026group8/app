43fa0bf1fb0bb6ecacb45b4262eb09f1f8c57c11
Daniel <250892580+danielohlord@users.noreply.github.com>
2026-07-21 18:46:38 -0400

    fix: removed unecessary public access modifier in UserAuthentication interface

--- app/src/main/java/com/cscb07/taamapp/auth/UserAuthentication.java
+++ app/src/main/java/com/cscb07/taamapp/auth/UserAuthentication.java
@@ -1,6 +1,6 @@
 package com.cscb07.taamapp.auth;
 
 public interface UserAuthentication {
-    //public void loadHome();
-    public void showError(String m);
+    //void loadHome();
+    void showError(String m);
 }

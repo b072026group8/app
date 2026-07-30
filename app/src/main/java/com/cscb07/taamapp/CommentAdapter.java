2dc75fcf873cc38d59c129a2b54fdc96362bb970
RichardGProj <richardgao783@gmail.com>
2026-07-30 15:54:48 -0400

    feat: add comment layout, textview updating to adapter, comment id and author id fields to comment data model

--- app/src/main/java/com/cscb07/taamapp/CommentAdapter.java
+++ app/src/main/java/com/cscb07/taamapp/CommentAdapter.java
@@ -1,7 +1,9 @@
 package com.cscb07.taamapp;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +12,6 @@ import java.util.List;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
     private List<Comment> commentList;
-
     public CommentAdapter(List<Comment> commentList) {
         this.commentList = commentList;
     }
@@ -18,12 +19,15 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     @NonNull
     @Override
     public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_comment_adapter, parent, false);
+        return new CommentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
-
+        Comment comment = this.commentList.get(position);
+        holder.commentName.setText(comment.getName());
+        holder.commentText.setText(comment.getText());
     }
 
     @Override
@@ -32,9 +36,11 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     }
 
     public static class CommentViewHolder extends RecyclerView.ViewHolder {
-
+        TextView commentName, commentText;
         public CommentViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.commentName = itemView.findViewById(R.id.commentName);
+            this.commentText = itemView.findViewById(R.id.commentText);
         }
     }
 }

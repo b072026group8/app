100a4b7b4a5654c1db9c41923a4cd4af3af320d7
RichardGProj <richardgao783@gmail.com>
2026-07-30 15:54:48 -0400

    feat: add comment layout, textview updating to adapter, comment id and author id fields to comment data model

--- app/src/main/java/com/cscb07/taamapp/Comment.java
+++ app/src/main/java/com/cscb07/taamapp/Comment.java
@@ -1,18 +1,33 @@
 package com.cscb07.taamapp;
 
 public class Comment {
+    private String id;
+    private String userUid;
     private String name;
     private String text;
 
     public Comment() {
+        this.id = "";
+        this.userUid = "";
         this.name = "";
         this.text = "";
     }
 
-    public Comment(String name, String text) {
+    public Comment(String id, String userUid, String name, String text) {
+        this.id = id;
+        this.userUid = userUid;
         this.name = name;
         this.text = text;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
 
     public String getName() {
         return name;

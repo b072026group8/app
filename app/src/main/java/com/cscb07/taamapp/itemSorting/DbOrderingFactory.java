1d9e40f021bc4e253d19ec44a0224d15e079f36c
deon-l <deon.liu@outlook.com>
2026-07-31 10:01:06 -0400

    refactor: Remove `ServiceProvider`.
    Given the deadlines, I don't think introducing this is worth it.

--- app/src/main/java/com/cscb07/taamapp/itemSorting/DbOrderingFactory.java
+++ app/src/main/java/com/cscb07/taamapp/itemSorting/DbOrderingFactory.java
@@ -4,13 +4,11 @@ import com.cscb07.taamapp.Item;
 import com.cscb07.taamapp.ItemMapProvider;
 import com.cscb07.taamapp.namedAlias.ItemMappingProvider;
 import com.cscb07.taamapp.util.Provider;
-import com.cscb07.taamapp.util.ServiceProvider;
 
 import java.util.List;
 
 /**
  * Provides an ordering of items, fetching items from a database
- * (specifically an instance of {@link ItemMappingProvider} provided as a service via {@link ServiceProvider}).
  */
 public class DbOrderingFactory implements OrderingFactory {
     @Override

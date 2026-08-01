d7c80c5e29c1178d14bca78124a6bdeb86cc2ac4
Zac <zachary.luozhejun@gmail.com>
2026-08-01 02:05:27 -0400

    Display artifact images in home and expanded views (#34)
    Co-authored-by: zacharyloh <zhejun.luo@mail.utoronto.ca>

--- app/src/main/java/com/cscb07/taamapp/ExpandedArtifactViewFragment.java
+++ app/src/main/java/com/cscb07/taamapp/ExpandedArtifactViewFragment.java
@@ -28,6 +28,9 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+
 
 public class ExpandedArtifactViewFragment extends Fragment {
     private final String Tag = "ExpandedArtifactViewFragment";
@@ -46,6 +49,7 @@ public class ExpandedArtifactViewFragment extends Fragment {
         ref = db.getReference("artifacts").child(lot);
         Button deleteButton = view.findViewById(R.id.artifactDelete);
         TextView name = view.findViewById(R.id.name);
+        ImageView imageView = view.findViewById(R.id.imageView5);
         TextView lotNum = view.findViewById(R.id.Lotnum);
         TextView description = view.findViewById(R.id.description);
         TextView category = view.findViewById(R.id.category);
@@ -89,6 +93,11 @@ public class ExpandedArtifactViewFragment extends Fragment {
                     provenance.setText(item.getProvenance());
                     accessionNumber.setText(item.getAccessionNumber());
                     notes.setText(item.getNotes());
+                    if (item.getImage() != null && !item.getImage().isEmpty()) {
+                        Glide.with(requireContext())
+                                .load(item.getImage())
+                                .into(imageView);
+                    }
 
                     if(!item.getCulturalOrigin().isEmpty()) {
                         culturalOriginHeader.setVisibility(View.VISIBLE);
